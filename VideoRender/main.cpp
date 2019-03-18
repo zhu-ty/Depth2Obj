2c31e2b09bb1e9d7059ec75cb5724c7d78710935
zhu-ty <zhu-ty@outlook.com>
2019-03-18 11:24:19 +0800

    video render

--- VideoRender/main.cpp
+++ VideoRender/main.cpp
@@ -16,45 +16,43 @@
 #include "VideoRender.h"
 #include "INIReader.h"
 
+std::vector<std::string> CollectFiles(std::string dir, std::vector<std::string> allowedExtensions)
+{
+	std::vector<std::string> ret;
+	//std::vector<std::string> allowedExtensions = { ".jpg", ".png" ,".jpeg" };
+	for (int i = 0; i < allowedExtensions.size(); i++) {
+		std::vector<cv::String> imageNamesCurrentExtension;
+		cv::glob(
+			dir + "/*" + allowedExtensions[i],
+			imageNamesCurrentExtension,
+			true
+		);
+		ret.insert(
+			ret.end(),
+			imageNamesCurrentExtension.begin(),
+			imageNamesCurrentExtension.end()
+		);
+	}
+	return ret;
+}
+
+
 int main(int argc, char* argv[])
 {
 	INIReader reader("OVConfig.ini");
 	
 	std::string color_dir = reader.Get("All", "Color", "./color");
 	std::string dis_dir = reader.Get("All", "Disparity", "./dis");
+	std::string output_name = reader.Get("All", "Output", "Video.h265");
 
 	std::vector<std::string> color_names, dis_names;
+	color_names = CollectFiles(color_dir, std::vector<std::string>({ ".jpg", ".png" ,".jpeg" }));
+	dis_names = CollectFiles(dis_dir, std::vector<std::string>({ ".tiff" }));
+
+	if (dis_names.size() == 1)
 	{
-		std::vector<std::string> allowedExtensions = { ".jpg", ".png" ,".jpeg" };
-		for (int i = 0; i < allowedExtensions.size(); i++) {
-			std::vector<cv::String> imageNamesCurrentExtension;
-			cv::glob(
-				color_dir + "/*" + allowedExtensions[i],
-				imageNamesCurrentExtension,
-				true
-			);
-			color_names.insert(
-				color_names.end(),
-				imageNamesCurrentExtension.begin(),
-				imageNamesCurrentExtension.end()
-			);
-		}
-	}
-	{
-		std::vector<std::string> allowedExtensions = { ".tiff" };
-		for (int i = 0; i < allowedExtensions.size(); i++) {
-			std::vector<cv::String> imageNamesCurrentExtension;
-			cv::glob(
-				dis_dir + "/*" + allowedExtensions[i],
-				imageNamesCurrentExtension,
-				true
-			);
-			dis_names.insert(
-				dis_names.end(),
-				imageNamesCurrentExtension.begin(),
-				imageNamesCurrentExtension.end()
-			);
-		}
+		color_names.resize(100, color_names[0]);
+		dis_names.resize(100, dis_names[0]);
 	}
 
 	ObjectVideoRender ovr;
@@ -74,16 +72,15 @@ int main(int argc, char* argv[])
 		ovr.RenderFrame(c, dis, cam, recall);
 		//cam.ProcessMouseMovement(-2, 0);
 		if(i < 4)
-			cam.ProcessKeyboard(SKOpenGL::camera::FORWARD, 35);
-		if ((i / 10) % 2 == 0)
-			cam.ProcessKeyboard(SKOpenGL::camera::LEFT, 3.5f);
+			cam.ProcessKeyboard(SKOpenGL::camera::FORWARD, 30);
+		if ((((i / 7) % 4 + 1) / 2) % 2 == 0)
+			cam.ProcessKeyboard(SKOpenGL::camera::RIGHT, 5.0f);
 		else
-			cam.ProcessKeyboard(SKOpenGL::camera::RIGHT, 3.5f);
+			cam.ProcessKeyboard(SKOpenGL::camera::LEFT, 5.0f);
 		SKCommon::infoOutput(SKCommon::format("%d", i));
 	}
 	//ovr.SaveImgs();
-	ovr.SaveVideos();
+	ovr.SaveVideos(output_name);
 	//system("pause");
-
 	return 0;
 }
